a7fc7abedaca790dc269cdd9bb716605bb12a4ab
Lora Zubic <lorazubic@Loras-MacBook-Pro.local>
2023-12-28 13:20:55 +0100

    matrix graph insertion added

--- Isomorphism-app/Isomorphism-app/Controllers/MatrixViewController.swift
+++ Isomorphism-app/Isomorphism-app/Controllers/MatrixViewController.swift
@@ -1,10 +1,162 @@
 import UIKit
+import PureLayout
 
 class MatrixViewController: UIViewController {
-
+    
+    var matrixSizeFieldOne: UITextField!
+    var matrixSizeFieldTwo: UITextField!
+    var inputMatrixViewOne: UIView!
+    var inputMatrixViewTwo: UIView!
+    var generateButton: UIButton!
+    var processButton: UIButton!
+    
+    var graphOne = Graph()
+    var graphTwo = Graph()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        setupMatrixSizeFields()
+    }
+    
+    func setupMatrixSizeFields() {
+        matrixSizeFieldOne = createMatrixSizeField(placeholder: "Enter size for graph 1")
+        matrixSizeFieldTwo = createMatrixSizeField(placeholder: "Enter size for graph 2")
+        
+        view.addSubview(matrixSizeFieldOne)
+        view.addSubview(matrixSizeFieldTwo)
+        
+        matrixSizeFieldOne.autoPinEdge(toSuperviewEdge: .top, withInset: 100)
+        matrixSizeFieldOne.autoAlignAxis(toSuperviewAxis: .vertical)
+        
+        matrixSizeFieldTwo.autoPinEdge(.top, to: .bottom, of: matrixSizeFieldOne, withOffset: 20)
+        matrixSizeFieldTwo.autoAlignAxis(toSuperviewAxis: .vertical)
+        
+        // Add a button to generate matrix input fields
+        generateButton = UIButton()
+        generateButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        generateButton.backgroundColor = UIColor.systemBlue
+        generateButton.setTitleColor(.white, for: .normal)
+        generateButton.layer.cornerRadius = 10
+        generateButton.setTitle("Generate matrix fields", for: .normal)
+        generateButton.layer.shadowColor = UIColor.black.cgColor
+        generateButton.layer.shadowOffset = CGSize(width: 0, height: 4)
+        generateButton.layer.shadowRadius = 5
+        generateButton.layer.shadowOpacity = 0.5
+        generateButton.layer.masksToBounds = false
+        generateButton.addTarget(self, action: #selector(generateMatrixInputFields), for: .touchUpInside)
+        view.addSubview(generateButton)
+        
+        generateButton.autoPinEdge(.top, to: .bottom, of: matrixSizeFieldTwo, withOffset: 30)
+        generateButton.autoPinEdge(.leading, to: .leading, of: view, withOffset: 20)
+        generateButton.autoPinEdge(.trailing, to: .trailing, of: view, withOffset: -20)
+        generateButton.autoSetDimension(.height, toSize: 50)
+    }
+    
+    func createMatrixSizeField(placeholder: String) -> UITextField {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = placeholder
+        textField.keyboardType = .numberPad
+        return textField
+    }
+    
+    @objc func generateMatrixInputFields() {
+        guard let sizeOneText = matrixSizeFieldOne.text,
+              let sizeOne = Int(sizeOneText),
+              let sizeTwoText = matrixSizeFieldTwo.text,
+              let sizeTwo = Int(sizeTwoText) else {
+            // Handle error: Show alert or message to the user
+            return
+        }
+        
+        generateButton.isHidden = true
+        
+        // Create and display matrix input fields based on entered sizes
+        inputMatrixViewOne = createMatrixInputFields(size: sizeOne, tag: 100)
+        inputMatrixViewTwo = createMatrixInputFields(size: sizeTwo, tag: 200)
+        
+        view.addSubview(inputMatrixViewOne)
+        view.addSubview(inputMatrixViewTwo)
+        
+        // Setup constraints for matrix input views
+        inputMatrixViewOne.autoPinEdge(.top, to: .bottom, of: matrixSizeFieldTwo, withOffset: 20)
+        inputMatrixViewOne.autoAlignAxis(toSuperviewAxis: .vertical)
+        inputMatrixViewOne.autoSetDimensions(to: CGSize(width: CGFloat(sizeOne * 40), height: CGFloat(sizeOne * 40)))
+        
+        inputMatrixViewTwo.autoPinEdge(.top, to: .bottom, of: inputMatrixViewOne, withOffset: 20)
+        inputMatrixViewTwo.autoAlignAxis(toSuperviewAxis: .vertical)
+        inputMatrixViewTwo.autoSetDimensions(to: CGSize(width: CGFloat(sizeTwo * 40), height: CGFloat(sizeTwo * 40)))
+        
+        // Add a button to process the matrices
+        processButton = UIButton()
+        processButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        processButton.backgroundColor = UIColor.systemBlue
+        processButton.setTitleColor(.white, for: .normal)
+        processButton.layer.cornerRadius = 10
+        processButton.setTitle("Are isomorphic?", for: .normal)
+        processButton.layer.shadowColor = UIColor.black.cgColor
+        processButton.layer.shadowOffset = CGSize(width: 0, height: 4)
+        processButton.layer.shadowRadius = 5
+        processButton.layer.shadowOpacity = 0.5
+        processButton.layer.masksToBounds = false
+        processButton.addTarget(self, action: #selector(processInputMatrices), for: .touchUpInside)
+        view.addSubview(processButton)
+        
+        processButton.autoPinEdge(.top, to: .bottom, of: inputMatrixViewTwo, withOffset: 20)
+        processButton.autoPinEdge(.leading, to: .leading, of: view, withOffset: 20)
+        processButton.autoPinEdge(.trailing, to: .trailing, of: view, withOffset: -20)
+        processButton.autoSetDimension(.height, toSize: 50)
+    }
+    
+    func createMatrixInputFields(size: Int, tag: Int) -> UIView {
+        let matrixView = UIView()
+        
+        var previousTextField: UITextField?
+        
+        // Create matrix fields based on size
+        for i in 0..<size {
+            for j in 0..<size {
+                let textField = UITextField()
+                textField.borderStyle = .roundedRect
+                textField.keyboardType = .numberPad
+                textField.tag = tag + (i * size) + j
+                matrixView.addSubview(textField)
+                
+                // Setup constraints for textField
+                textField.autoSetDimensions(to: CGSize(width: 40, height: 40))
+                
+                if j == 0 { // first column
+                    textField.autoPinEdge(toSuperviewEdge: .leading)
+                } else {
+                    textField.autoPinEdge(.leading, to: .trailing, of: previousTextField!)
+                }
+                
+                if i == 0 { // first row
+                    textField.autoPinEdge(toSuperviewEdge: .top)
+                } else {
+                    // Align textField to the one above it
+                    if let aboveTextField = matrixView.viewWithTag(textField.tag - size) as? UITextField {
+                        textField.autoPinEdge(.top, to: .bottom, of: aboveTextField)
+                    }
+                }
+                
+                previousTextField = textField
+                
+                if j == size - 1 { // last column
+                    textField.autoPinEdge(toSuperviewEdge: .trailing)
+                }
+                
+                if i == size - 1 { // last row
+                    textField.autoPinEdge(toSuperviewEdge: .bottom)
+                }
+            }
+            previousTextField = nil // reset for the next row
+        }
+        return matrixView
+    }
+    
+    @objc func processInputMatrices() {
+        
     }
 }
