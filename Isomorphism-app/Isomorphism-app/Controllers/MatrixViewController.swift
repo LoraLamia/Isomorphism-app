e3930f72913b7912ff1b496266cf55e86ca385fa
Lora Zubic <lorazubic@Loras-MacBook-Pro.local>
2024-03-07 06:26:53 +0100

    picker added to MatrixViewcontroller

--- Isomorphism-app/Isomorphism-app/Controllers/MatrixViewController.swift
+++ Isomorphism-app/Isomorphism-app/Controllers/MatrixViewController.swift
@@ -10,6 +10,7 @@ class MatrixViewController: UIViewController, UITextFieldDelegate {
     var generateButton: UIButton!
     var processButton: UIButton!
     var scrollView: UIScrollView!
+    let popupView = PopUpView()
     
     var graphOne: Graph!
     var graphTwo: Graph!
@@ -17,11 +18,11 @@ class MatrixViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        scrollViewSetUp()
+        scrollAndPopupViewSetUp()
         setupMatrixSizeFields()
     }
     
-    private func scrollViewSetUp() {
+    private func scrollAndPopupViewSetUp() {
         scrollView = UIScrollView()
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(scrollView)
@@ -31,6 +32,12 @@ class MatrixViewController: UIViewController, UITextFieldDelegate {
             scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
+        
+        popupView.confirmButton.addTarget(self, action: #selector(processInputMatrices), for: .touchUpInside)
+        
+        view.addSubview(popupView)
+        popupView.isHidden = true
+        popupView.autoPinEdgesToSuperviewEdges()
     }
     
     func setupMatrixSizeFields() {
@@ -173,7 +180,7 @@ class MatrixViewController: UIViewController, UITextFieldDelegate {
         processButton.layer.shadowRadius = 5
         processButton.layer.shadowOpacity = 0.5
         processButton.layer.masksToBounds = false
-        processButton.addTarget(self, action: #selector(processInputMatrices), for: .touchUpInside)
+        processButton.addTarget(self, action: #selector(checkIsomorphism), for: .touchUpInside)
         scrollView.addSubview(processButton)
         
         processButton.autoPinEdge(.top, to: .bottom, of: inputMatrixViewTwo, withOffset: 20)
@@ -248,6 +255,10 @@ class MatrixViewController: UIViewController, UITextFieldDelegate {
 
         self.view.layoutIfNeeded()
     }
+    
+    @objc func checkIsomorphism() {
+        popupView.isHidden = false
+    }
 
     @objc func processInputMatrices() {
         guard let sizeOne = matrixSizeFieldOne.text.flatMap(Int.init),
@@ -266,13 +277,30 @@ class MatrixViewController: UIViewController, UITextFieldDelegate {
         graphOne.cleanUpDoubleEdges()
         graphTwo.cleanUpDoubleEdges()
         
-        let alg = AlgorithmInvariantInducingFunctions(graphOne: graphOne, graphTwo: graphTwo)
+        let selectionIndex = popupView.picker.selectedRow(inComponent: 0)
+        let selection = popupView.choices[selectionIndex]
         
-        let message = alg.areIsomorphic() ? "Graphs are isomorphic!" : "Graphs are NOT isomorphic!"
+        var alg: GraphIsomorphismAlgorithm?
+        var message: String?
+        if(selection == "Invarijants algorithm") {
+            alg = AlgorithmInvariantInducingFunctions(graphOne: graphOne, graphTwo: graphTwo)
+        } else if(selection == "Certificates algorithm") {
+            if(!graphOne.isTree() || !graphTwo.isTree()) {
+            } else {
+                alg = AlgorithmCertificates(graphOne: graphOne, graphTwo: graphTwo)
+            }
+        }
+        
+        if let alg = alg {
+            message = alg.areIsomorphic() ? "Graphs are isomorphic!" : "Graphs are NOT isomorphic!"
+        } else {
+            message = "Both graphs need to be trees!"
+        }
+        
         let alert = UIAlertController(title: "", message: message, preferredStyle: .alert)
         
         let attributedMessage = NSMutableAttributedString(
-            string: message,
+            string: message ?? "",
             attributes: [
                 NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18)
             ]
@@ -285,6 +313,8 @@ class MatrixViewController: UIViewController, UITextFieldDelegate {
         }
         alert.addAction(okAction)
         self.present(alert, animated: true, completion: nil)
+        
+        popupView.isHidden = true
     }
     
     func parseMatrix(from matrixView: UIView, size: Int, startingTag: Int) -> [[Int]] {
