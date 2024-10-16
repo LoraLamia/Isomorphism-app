1085cbc0d0757255b4b8db90ffd0f4302c0bb453
Lora Zubic <lorazubic@Loras-MacBook-Pro.local>
2024-10-16 15:15:59 +0200

    changes

--- Isomorphism-app/Isomorphism-app/Algorithms/AlgorithmCertificatesGraphs.swift
+++ Isomorphism-app/Isomorphism-app/Algorithms/AlgorithmCertificatesGraphs.swift
@@ -23,16 +23,25 @@ class AlgorithmCertificatesGraphs: GraphIsomorphismAlgorithm {
         var partition: [[Vertex]] = []
         var block = [Vertex]()
         
-        for i in 0..<graph.vertices.count {
+        block.append(graph.vertices[0])
+        var block2 = [Vertex]()
+        for i in 1..<graph.vertices.count {
             let vertex = graph.vertices[i]
-            block.append(vertex)
+            block2.append(vertex)
         }
-        
         partition.append(block)
+        partition.append(block2)
+        
+//        for i in 0..<graph.vertices.count {
+//            let vertex = graph.vertices[i]
+//            block.append(vertex)
+//        }
+//        
+//        partition.append(block)
         return partition
     }
     
-    func refine(G: Graph, A: [[Vertex]]) {
+    func refine(G: Graph, A: [[Vertex]]) -> [[Vertex]] {
         var B = A
         var S = [[Vertex]]()
         
@@ -81,6 +90,45 @@ class AlgorithmCertificatesGraphs: GraphIsomorphismAlgorithm {
         print("Konačna ekvidistantna particija B: \(B.map { $0.map { $0.id } })")
         print("Konačna ekvidistantna particija S: \(S.map { $0.map { $0.id } })")
         print("--------------")
+        return B
     }
     
+    func compare(G: Graph, μ: [[Vertex]], π: [[Vertex]], n: Int) -> ComparisonResult {
+        for j in 1..<n {
+            for i in 0..<j {
+                let x = G.areAdjacent(μ[i][0], μ[j][0]) ? 1 : 0
+                let y = G.areAdjacent(π[i][0], π[j][0]) ? 1 : 0
+                
+                if x < y {
+                    return .worse
+                } else if x > y {
+                    return .better
+                }
+            }
+        }
+        return .equal
+    }
+    
+    func canon(G: Graph, P: [[Vertex]]) -> [[Vertex]] {
+        var Q = refine(G: G, A: P)
+        
+        var bestExists = false
+        var bestPartition = Q
+        
+        //index = Q.firstIndex(where: { $0.count > 1 })
+        
+        return [[Vertex]]()
+    }
+    
+    
+    
 }
+
+
+enum ComparisonResult {
+    case better
+    case worse
+    case equal
+}
+
+
